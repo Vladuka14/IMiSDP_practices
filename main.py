8cbe79d89eb5cc93c915e3f957c2beef5aa9418e
Vladislav Sakharov <43438730+Vladuka14@users.noreply.github.com>
2023-05-22 12:02:38 +0300

    print description

--- main.py
+++ main.py
@@ -6,4 +6,6 @@ import string
 id = 41450002
 # Запрос страницы из базы данных api и форматирование в объект словаря python
 vacancy = requests.get("https://api.hh.ru/vacancies/" + str(id)).json()
-print(vacancy)
+
+description = html_cleaner(vacancy['description'])
+print(description)
