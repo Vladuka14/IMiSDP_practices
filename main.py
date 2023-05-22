6c473796dc9b62b3d80801998d122dfee61d1065
Vladislav Sakharov <43438730+Vladuka14@users.noreply.github.com>
2023-05-22 11:57:53 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import re
+import requests
+import json
+import string
+
+id = 41450002
+# Запрос страницы из базы данных api и форматирование в объект словаря python
+vacancy = requests.get("https://api.hh.ru/vacancies/" + str(id)).json()
+print(vacancy)
